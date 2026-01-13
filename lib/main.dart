b148f6e5696154ebc904eb009bd4fde9aa889a47
zhangyuxian <zhangyuxian1014@gmail.com>
2026-01-13 22:32:25 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 34, 56, 255)),
+              seedColor: const Color.fromARGB(255, 45, 15, 214)),
         ),
         home: MyHomePage(),
       ),
@@ -32,24 +32,57 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>{};
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current; // ← Add this.
-
     return Scaffold(
-      body: Column(
+      body: Row(
         children: [
-          Nice1(),
-          Text(pair.asLowerCase), // ← Change to this.
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
+          SafeArea(
+            child: NavigationRail(
+              extended: true,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: InteractionPage1(),
+            ),
           ),
         ],
       ),
@@ -57,10 +90,50 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class Nice1 extends StatelessWidget {
-  const Nice1({
+class InteractionPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData favor = appState.favorites.contains(pair)
+        ? Icons.favorite
+        : Icons.favorite_border;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 20),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: appState.getNext,
+                child: Text('Next'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton.icon(
+                onPressed: appState.toggleFavorite,
+                icon: Icon(favor),
+                label: Text('Like'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
     super.key,
+    required this.pair,
   });
+
+  final WordPair pair;
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +142,10 @@ class Nice1 extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text('A random AWESOME idea:'),
+        child: Text(
+          pair.asLowerCase,
+          style: TextStyle(fontSize: 40),
+        ),
       ),
     );
   }
